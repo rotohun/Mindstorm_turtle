2ae6c67852a305df51d82e1d79496c3ea2c1ed57
Ronald Hunter <ronaldtoddhunter@yahoo.com>
2017-10-07 15:04:43 -0400

    Made moving function for the turtle

---
+++ mindstorm.py
@@ -0,0 +1,25 @@
+import turtle
+
+def draw_square():
+    #Window for drawing
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    #brad is moving
+    brad = turtle.Turtle()
+    brad.forward(100)
+    brad.right(90)
+
+    brad.forward(100)
+    brad.right(90)
+
+    brad.forward(100)
+    brad.right(90)
+
+    brad.forward(100)
+    brad.right(90)
+
+
+    window.exitonclick()
+
+draw_square()
