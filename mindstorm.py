0c1304d582f4fe1f5bc9167f796c68d288ae4e52
Ronald Hunter <ronaldtoddhunter@yahoo.com>
2017-10-08 15:11:54 -0400

    updated to brad and angie code

--- mindstorm.py
+++ mindstorm.py
@@ -1,30 +1,32 @@
 import turtle
 
-def draw_square():
-    #Window for drawing
+
+
+
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
-
-    #brad is moving
+    #Brad
     brad = turtle.Turtle()
-    turtle.shape("turtle")
-    turtle.pencolor("green")
-    turtle.speed(9)
-    
-    
-    brad.forward(100)
-    brad.right(90)
-
-    brad.forward(100)
-    brad.right(90)
-
-    brad.forward(100)
-    brad.right(90)
-
-    brad.forward(100)
-    brad.right(90)
-
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+    draw_square(brad)
+    #Angie
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
 
     window.exitonclick()
+    
+    
 
-draw_square()
+
+draw_art()
